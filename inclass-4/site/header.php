07916300efc79160f3dcd30760982c23acaa14ee
scha0256 <77338543+scha0256@users.noreply.github.com>
2022-02-23 15:49:09 -0500

    Add files via upload

---
+++ inclass-4/site/header.php
@@ -0,0 +1,26 @@
+ <!-- Add your content of header -->
+ <header>
+  <nav class="navbar  navbar-fixed-top navbar-default">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle uarr collapsed" data-toggle="collapse" data-target="#navbar-collapse-uarr">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="./index.html" title="">
+          <img src="./assets/apple-icon-180x180.png" class="navbar-logo-img" alt="">
+        </a>
+      </div>
+
+      <div class="collapse navbar-collapse" id="navbar-collapse-uarr">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="./index.php" title="" class="active">Home</a></li>
+          <li><a href="./about.php" title=""> About</a></li>
+          <li><a href="./pricing.php" title=""> Pricing </a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
---
+++ inclass-4/site/index.php
@@ -0,0 +1,127 @@
+<?php 
+    // Including the header
+    include "header.php"; 
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta content="IE=edge" http-equiv="X-UA-Compatible">
+  <meta content="width=device-width,initial-scale=1" name="viewport">
+  <meta content="description" name="description">
+  <meta name="google" content="notranslate" />
+  <meta content="Mashup templates have been developped by Orson.io team" name="author">
+
+  <!-- Disable tap highlight on IE -->
+  <meta name="msapplication-tap-highlight" content="no">
+  
+  <link href="./assets/apple-icon-180" rel="apple-touch-icon">
+  <link href="./assets/favicon.icon" rel="icon">
+
+ 
+
+  <title>Title page</title>  
+
+<link href="./assets/css/main.a3f694c0.css" rel="stylesheet"></head>
+
+<body>
+
+
+
+
+
+<div class="white-text-container background-image-container" style="background-image: url('./assets/images/img-home.jpg')">
+    <div class="opacity"></div>
+    <div class="container">
+        <div class="row">
+           
+            <div class="col-md-6">
+                <h1>Posuere lorem Ipsum</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Adipiscing commodo elit at imperdiet dui accumsan sit. Ipsum dolor sit
+                    amet consectetur adipiscing elit. </p>
+                 <a href="./about.html" title="" class="btn btn-lg btn-primary">About Us</a>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<div class="section-container border-section-container">
+    <div class="container">
+            <div class="row">
+                <div class="col-md-12 section-container-spacer">
+                    <div class="text-center">
+                        <h2>Vivamus laoreet</h2>
+                        <p> Auctor augue mauris augue neque. Posuere lorem ipsum dolor sit amet consectetur adipiscing.<br> Porta non
+                            pulvinar neque laoreet. Viverra ipsum nunc aliquet bibendum. </p>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="fa-container">
+                        <i class="fa fa-comment-o fa-3x" aria-hidden="true"></i>
+                    </div>
+                    <div class="text-center">
+                        <h3>Consectetur</h3>
+                    </div>
+                    <div>
+                        <p>Auctor augue mauris augue neque. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Porta
+                            non pulvinar neque laoreet. Viverra ipsum nunc aliquet bibendum. Iaculis urna id volutpat lacus.
+                            Turpis egestas pretium aenean pharetra magna ac.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="fa-container">
+                        <i class="fa fa-heart-o fa-3x" aria-hidden="true"></i>
+                    </div>
+                    <div class="text-center">
+                        <h3>Malesuada</h3>
+                    </div>
+                    <div>
+                        <p>Auctor augue mauris augue neque. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Porta
+                            non pulvinar neque laoreet. Viverra ipsum nunc aliquet bibendum. Iaculis urna id volutpat lacus.
+                            Turpis egestas pretium aenean pharetra magna ac.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="fa-container">
+                        <i class="fa fa-bell-o fa-3x" aria-hidden="true"></i>
+                    </div>
+                    <div class="text-center">
+                        <h3>Phasellus</h3>
+                    </div>
+                    <div>
+                        <p>Auctor augue mauris augue neque. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Porta
+                            non pulvinar neque laoreet. Viverra ipsum nunc aliquet bibendum. Iaculis urna id volutpat lacus.
+                            Turpis egestas pretium aenean pharetra magna ac. 
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+    </div>
+</div>
+
+<div class="section-container background-color-container white-text-container">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="text-center">
+                    <h2>Vivamus laoreet</h2>
+                    <p> Auctor augue mauris augue neque. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Porta non
+                        pulvinar neque laoreet. Viverra ipsum nunc aliquet bibendum. Iaculis urna id volutpat lacus. Turpis
+                        egestas pretium aenean pharetra magna ac. Id cursus metus aliquam eleifend mi. </p>
+                </div>
+            </div>
+        </div>
+     </div>
+ </div>
+
+ <?php 
+    // Including the header
+    include "footer.php"; 
+?>
+</html>
